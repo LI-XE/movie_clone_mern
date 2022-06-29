0a0e705ba836574d2ae32c250133c6599e67d215
LI_XE <hyangeun312@gmail.com>
2022-06-29 01:45:16 -0500

    update

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -11,7 +11,7 @@ router.post("/saveComment", (req, res) => {
     if (err) return res.json({ success: false, err });
 
     Comment.find({ _id: comment._id })
-      .populate((writer = req.body.userFrom))
+      .populate("writer")
       .exec((err, result) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).json({ success: true, result });
@@ -22,11 +22,19 @@ router.post("/saveComment", (req, res) => {
 
 router.post("/getComments", (req, res) => {
   Comment.find({ postId: req.body.movieId })
-    .populate((writer = req.body.userFrom))
+    .populate("writer")
     .exec((err, comments) => {
       if (err) return res.status(400).send(err);
       res.status(200).json({ success: true, comments });
     });
+});
+
+router.delete("/deleteComment", authenticate, (req, res) => {
+  console.log(req.body._id);
+  Comment.findOneAndDelete({ _id: req.body._id }).exec((err, doc) => {
+    if (err) return res.status(400).json({ success: false, err });
+    res.status(200).json({ success: true, doc });
+  });
 });
 
 module.exports = router;
